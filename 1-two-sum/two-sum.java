6b7af39f54706013d4f385c694882065e1ac0344
Shaik Rezwan Ahmmed Rafi <onlyonerafi@gmail.com>
2026-07-20 20:47:02 +0600

    Time: 45 ms (26.36%) | Memory: 47.1 MB (40.20%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,14 +1,5 @@
 class Solution {
-    // The JVM Warmup H@ck
-    static {
-        int[] dummyArray = {0, 0};
-        for(int i = 0; i < 1000; i++) {
-            // Force the JIT compiler to optimize the method before the timer starts
-            int[] dummyResult = twoSum(dummyArray, 0); 
-        }
-    }
-
-    public static int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
                 if(nums[i]+nums[j]==target){
@@ -16,6 +7,7 @@ class Solution {
                 }
             }
         }
-        return new int[]{-1,-1}; //just return if no solution found
+        return new int[]{-1,-1}; //just retun if no solution found
+
     }
 }
